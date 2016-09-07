e659fe3e91834961470be2fe2b2f2d92dba0504f
zhujiajun <zhujiajun.me@gmail.com>
2016-09-07 21:09:56 +0800

    simplelogistic

--- regression/src/main/java/com/github/zjiajun/regression/logistic/SimpleLogistic.java
+++ regression/src/main/java/com/github/zjiajun/regression/logistic/SimpleLogistic.java
@@ -18,15 +18,22 @@ public class SimpleLogistic {
     private double[] weights;
 
     /** the number of iterations */
-    private int ITERATIONS = 3000;
+    private int ITERATIONS = 3000_00;
 
     public SimpleLogistic(int n) {
         this.rate = 0.0001;
         weights = new double[n];
     }
 
-    private static double sigmoid(double z) {
-        return 1.0 / (1.0 + Math.exp(-z));
+    public double computeCost(List<Instance> instances) {
+        double lik = 0.0;
+        for (int i=0; i<instances.size(); i++) {
+            double[] x = instances.get(i).x;
+            double predicted = classify(x);
+            double label = instances.get(i).label;
+            lik += label * Math.log(predicted) + (1 - label) * Math.log(1 - predicted);
+        }
+        return lik;
     }
 
     public void train(List<Instance> instances) {
@@ -54,6 +61,10 @@ public class SimpleLogistic {
         return sigmoid(logit);
     }
 
+    private static double sigmoid(double z) {
+        return 1.0 / (1.0 + Math.exp(-z));
+    }
+
     public static class Instance {
         public double label;
         public double [] x;
@@ -64,19 +75,18 @@ public class SimpleLogistic {
         }
     }
 
-
-
     public static List<Instance> readData(String file) throws IOException {
-        List<Instance> dataset = new ArrayList<Instance>();
+        List<Instance> dataset = new ArrayList<>();
         String str;
         BufferedReader reader = null;
         try {
             reader = new BufferedReader(new FileReader(file));
             while ((str = reader.readLine()) != null) {
                 String[] split = str.split(",");
-                double[] data = new double[split.length - 1];
-                data[0] = Double.parseDouble(split[0]);
-                data[1] = Double.parseDouble(split[1]);
+                double[] data = new double[split.length];
+                data[0] = 1.0;
+                data[1] = Double.parseDouble(split[0]);
+                data[2] = Double.parseDouble(split[1]);
                 double label = Double.parseDouble(split[2]);
                 Instance instance = new Instance(label, data);
                 dataset.add(instance);
@@ -88,7 +98,7 @@ public class SimpleLogistic {
     }
 
     public static List<Instance> readDataSet(String file) throws FileNotFoundException {
-        List<Instance> dataset = new ArrayList<Instance>();
+        List<Instance> dataset = new ArrayList<>();
         Scanner scanner = null;
         try {
             scanner = new Scanner(new File(file));
@@ -119,17 +129,28 @@ public class SimpleLogistic {
 
     public static void main(String... args) throws IOException {
         String file = Thread.currentThread().getContextClassLoader().getResource("ex2data1.txt").getFile();
-        System.out.println(file);
 //        List<Instance> instances = readDataSet(file);
         List<Instance> instances = readData(file);
-        SimpleLogistic logistic = new SimpleLogistic(2);
+        SimpleLogistic logistic = new SimpleLogistic(3);
+        double cost = logistic.computeCost(instances);
+        System.out.println("Cost value : " + cost / instances.size());
+
         logistic.train(instances);
-        double[] x = {2, 1, 1, 0, 1};
+
+        double[] x = {1,0,34.62365962451697,78.0246928153624};
         System.out.println("prob(1|x) = " + logistic.classify(x));
 
-        double[] x2 = {1, 0, 1, 0, 0};
-//        double[] x2 = {1, 0, 1, 0, 0};
+        double[] x2 = {1.0,60.18259938620976,86.30855209546826};
         System.out.println("prob(1|x2) = " + logistic.classify(x2));
+
+        double[] x3 = {1.0,82.30705337399482,76.48196330235604};
+        System.out.println("prob(1|x3) = " + logistic.classify(x3));
+
+        double[] x4 = {1.0,69.36458875970939,97.71869196188608};
+        System.out.println("prob(1|x4) = " + logistic.classify(x4));
+
+        double[] x5 = {1.0,39.53833914367223,76.03681085115882};
+        System.out.println("prob(1|x5) = " + logistic.classify(x5));
 
     }
 }
